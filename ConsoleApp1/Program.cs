ecd855128ec013c4646e0b41d7b0de4081706b59
Joao Roberto <joao.rjunior87@gmail.com>
2018-09-19 20:24:51 -0300

    funcionando

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@ using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,26 +10,51 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+        public static RedisChannel canal = "perguntas";
+        public static IConnectionMultiplexer client = ConnectionMultiplexer.Connect("191.232.234.20");
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey(); 
+            var db = client.GetDatabase();
 
-            var redis = ConnectionMultiplexer.Connect("localhost");
-            var db = redis.GetDatabase(); 
-
-            db.StringSet("Ola", "Oi eu sei responder");
-            var value = db.StringGet("Ola");
-
-            //var pub = redis.GetSubscriber();
-            //pub.Publish("Perguntas", "Qual é a pergunta que ele vai fazer?");
-
-            var sub = redis.GetSubscriber();
-            sub.Subscribe("Perguntas", (channel, msg) =>
+            var sub = client.GetSubscriber();
+            sub.Subscribe(canal, (ch, msg) =>
             {
-                msg.ToString();
+                if (msg.ToString().StartsWith("P"))
+                    GeraResposta(msg.ToString());
             });
 
+            Console.ReadKey();
         }
+
+        public static void GeraResposta(string msg)
+        {
+            var pub = client.GetSubscriber();
+            var pergunta = msg.Split(':');
+            string x = pergunta[0].Substring(0, 1);
+
+            var resposta = int.Parse(x) + int.Parse(x); //getRespostaGoogle(msg);
+
+            var db = client.GetDatabase();
+            db.HashSet(pergunta[0], "Joao",resposta);
+        }
+
+        //public static string getRespostaGoogle(string msg)
+        //{
+        //    WebClient webClient = new WebClient();
+        //    webClient.Headers.Add("user-agent", "Only a test!");
+
+        //    string apiKey = "AIzaSyDiRCI-3Djsw8qTrXIYF7AIqnCSyyHmFmM";
+        //    string cx = "009431853094902135308:axmjfgvmaam";
+
+
+        //    //var results = webClient.DownloadString(String.Format("https://www.google.com.au/search?q={0}&alt=json", msg));
+        //    var results = webClient.DownloadString(String.Format("https://www.googleapis.com/customsearch/v1?key={0}&cx={1}&q={2}&alt=json", apiKey, cx, msg));
+
+        //    // web.Dispose();
+        //    return results;
+        //}
     }
 }
+
